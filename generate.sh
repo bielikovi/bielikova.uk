b1d2e46438e037b3e4c1d2491ff88134d825a4a2
Namdak Tonpa <maxim@synrc.com>
2026-04-23 17:52:22 +0300

    wip

--- generate.sh
+++ generate.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-# 1. Parse the GEDCOM file and extract data into the storage directory
-# -s: source GEDCOM file
-# -e: base directory for storage/static folders
 ruby parse_sokhatsky.rb -s bielikova.ged -e .
-
-# 2. Generate the static HTML pages and the index
-# -e: base directory for storage/static folders
-# --index: directory where the main index.html should be generated
 ruby genie_sokhatsky.rb -e . --index .
